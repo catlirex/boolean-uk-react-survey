03326985305ce64866931ce512f34eecf3f4f9f2
Catherine Li <catherine.uk.20@gmail.com>
2021-06-17 18:40:26 +0100

    complete daily

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ import ReviewForm from "./ReviewForm";
 export default function Main() {
   const [open, setOpen] = useState(false); //Ignore this state
   const [resultList, setResultList] = useState([])
-
+  
   return (
     <main className="main">
       <section className={`main__list ${open ? "open" : ""}`}>
